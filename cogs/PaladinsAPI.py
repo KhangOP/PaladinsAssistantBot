5b0fdb3f6e4f0bf0dea384ff7bfa0095b9ba3099
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-01 16:49:22 -0200

    Update PaladinsAPI.py

--- cogs/PaladinsAPI.py
+++ cogs/PaladinsAPI.py
@@ -205,6 +205,8 @@ class PaladinsAPICog(commands.Cog, name="Paladins API Commands"):
     # Gets KDA and Win Rate for a player from Nonsocial's herokuapp
     @classmethod
     async def get_global_kda(cls, player_id):
+        if str(player_id) == '0':
+            return ["Private Account", "???", "???", "???"]
         url = "http://nonsocial.herokuapp.com/api/kda?player=" + str(player_id)
         async with aiohttp.ClientSession(conn_timeout=5, read_timeout=5) as cs:
             async with cs.get(url) as r:

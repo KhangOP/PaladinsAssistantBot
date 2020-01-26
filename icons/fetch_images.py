b3b84e6d3f4bf91d37c327d929077b621a23c38a
EthanHicks1 <Cyanotrichite@gmail.com>
2020-01-26 18:11:07 -0500

    Adding tiberius json card data. Forgot to commit it last time. Also fixed mal-damba missing other languages in other card data.

--- icons/fetch_images.py
+++ icons/fetch_images.py
@@ -135,16 +135,18 @@ def save_card_descriptions(name):
     champion_card_descriptions = {}
     for lang_num in languages:
         lang = convert_language(lang_num)
-        if "Mal" in name:
+        if "mal" in name.lower():
             name = "maldamba"
 
         data_url = "https://cms.paladins.com/wp-json/wp/v2/champions?slug={}&lang_id={}".format(name, lang_num)
+        # print(data_url)
         json_data = requests.get(url=data_url)
 
         try:
             json_data = json_data.json()[0].get("cards")
+            # print(json_data)
         except (IndexError, json.decoder.JSONDecodeError):
-            print("The champion {} does not have any card data.".format(name))
+            print("The champion {} does not have any card data for the language {}.".format(name, lang))
             return None
 
         champion_card_descriptions[lang] = {}
@@ -182,11 +184,11 @@ for champ in all_champs:
     save_card_descriptions(name=champ_name)
 """
 
-new_champ = "tiberius"
+new_champ = "Mal'Damba"
 # save_champ_icons(new_champ)
 # save_champ_headers(new_champ)
-save_champ_cards(new_champ)
-# save_card_descriptions(new_champ)
+# save_champ_cards(new_champ)
+save_card_descriptions(new_champ)
 
 # for i in range(1, 6):
 #    url = "https://web2.hirez.com/paladins/cards/frame-{}.png".format(i)
